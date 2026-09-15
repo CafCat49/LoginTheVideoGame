a54cdea98d57fad5ec1d7104b338778c71b588ae
2lif3 <sondre.stensland@yahoo.com>
2026-09-15 19:02:20 +0200

    Added variables to change the behaviour of movement, in the editor

---
+++ source/Scripts/PlayerController.gd
@@ -0,0 +1,47 @@
+extends CharacterBody2D
+
+@export var speed: float = 300.0
+@export var jump_velocity: float = -400.0
+@export var has_double_jump: bool = true
+@export var flight_turn_speed: float = 100.0
+
+var last_direction: float = 0.0
+var extra_jump: bool = false
+
+func _init() -> void:
+	extra_jump = has_double_jump
+
+func _physics_process(delta: float) -> void:
+	# Add the gravity.
+	if not is_on_floor():
+		velocity += get_gravity() * delta
+
+	var direction := Input.get_axis("ui_left", "ui_right")
+	
+	# Handle jump.
+	if Input.is_action_just_pressed("ui_accept") and (is_on_floor() or extra_jump):
+		last_direction = direction
+		if is_on_floor() and has_double_jump:
+			extra_jump = true;
+		if not is_on_floor():
+			extra_jump = false;
+		velocity.y = jump_velocity
+
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var current_speed = speed
+	# print(str(int(velocity.x) % 2) + " " + str(int(last_direction) % 2))
+	var changed_direction = (velocity.x > 0 and last_direction < 0)   \
+						 or (velocity.x < 0 and last_direction > 0)   \
+						 or (velocity.x == 0 and last_direction != 0) \
+						 or (velocity.x != 0 and last_direction == 0)
+ 									# If you changed direction in the air
+	if not is_on_floor() and changed_direction:
+		current_speed = flight_turn_speed
+		
+	if direction:
+		velocity.x = direction * current_speed
+	else:
+		velocity.x = move_toward(velocity.x, 0, current_speed)
+
+	move_and_slide()
